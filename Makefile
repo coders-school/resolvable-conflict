cc148fd4aa84f2b45f9cace91ff1e893aeb9e27f
Łukasz Ziobroń <lukasz@coders.school>
2020-05-25 13:07:47 +0200

    Compilers flag exercise

---
+++ Makefile
@@ -0,0 +1,3 @@
+main: main.cpp functions.cpp functions.hpp
+	g++ -std=c++17 main.cpp functions.cpp -o main
+
---
+++ functions.hpp
@@ -0,0 +1,5 @@
+#include <vector>
+
+void print(std::vector<int> const &);
+void sort(std::vector<int> &);
+
