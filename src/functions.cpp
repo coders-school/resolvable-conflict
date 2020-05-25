cf7597a9e8976d443676f30ab9a8592353efbe99
Łukasz Ziobroń <lukasz@coders.school>
2020-05-25 13:25:40 +0200

    cherry-picked 3 commits

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,10 +1,8 @@
-#include "functions.hpp"  
-#include <algorithm>
+#include "../inc/functions.hpp"    // TODO: add proper compilation flag and change to #include "functions.hpp"
 #include <iostream>
-#include <limits>
 
 void print(std::vector<int> const & numbers) {
-    for (size_t i = 0; i < numbers.size(); ++i) {
+    for (int i = 0; i < numbers.size(); ++i) {
         std::cout << i << ": " << numbers[i] << '\n';
     }
     std::cout << '\n';
@@ -14,16 +12,3 @@ void sort(std::vector<int> & unsorted) {
     std::sort(begin(unsorted), end(unsorted));
 }
 
-int min(std::vector<int> const & values) {
-    int minimum = std::numeric_limits<int>::max();
-    for (const auto & value : values) {
-        if (value < minimum) {
-            minimum = value;
-        }
-    }
-    return minimum;
-}
-
-void reverse(std::vector<int> & reverseMe){
-    return std::reverse(reverseMe.begin(),reverseMe.end());
-}
