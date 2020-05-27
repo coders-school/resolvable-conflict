c0e0ace34d0949bf097ae908e3c49a5280538406
Karol Dulemba <kdulemba@gmail.com>
2020-05-27 23:26:11 +0200

    Ready for compilation +vimtutor

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,6 +1,6 @@
-#include "../inc/functions.hpp"    // TODO: add proper compilation flag and change to #include "functions.hpp"
+#include "functions.hpp"    // TODO: add proper compilation flag and change to #include "functions.hpp"
 #include <iostream>
-#include <limits>
+#include <algorithm>
 
 void print(std::vector<int> const & numbers) {
     for (int i = 0; i < numbers.size(); ++i) {
