20df3b2712eba68e6b8413748ccd7a619782310b
kozlofski <kozlofski.dev@gmail.com>
2020-05-28 15:00:12 +0200

    Vimtutor finished

--- src/functions.cpp
+++ src/functions.cpp
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
-#include "functions.hpp"
-#include <iostream>
-#include <limits>
-#include <algorithm>
-
-void print(std::vector<int> const & numbers) {
-    for (size_t i = 0; i < numbers.size(); ++i) {
-=======
 #include "../inc/functions.hpp"    // TODO: add proper compilation flag and change to #include "functions.hpp"
 #include <iostream>
 #include <limits>
 
 void print(std::vector<int> const & numbers) {
     for (int i = 0; i < numbers.size(); ++i) {
->>>>>>> a6c01ef... Better code layout
         std::cout << i << ": " << numbers[i] << '\n';
     }
     std::cout << '\n';
@@ -23,10 +13,6 @@ void sort(std::vector<int> & unsorted) {
     std::sort(begin(unsorted), end(unsorted));
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 132390b... New function - min
 int min(std::vector<int> const & values) {
     int minimum = std::numeric_limits<int>::max();
     for (const auto & value : values) {
@@ -36,13 +22,3 @@ int min(std::vector<int> const & values) {
     }
     return minimum;
 }
-
-<<<<<<< HEAD
-void reverse(std::vector<int> & data) {
-    std::reverse(begin(data), end(data));
-}
-
-=======
->>>>>>> a6c01ef... Better code layout
-=======
->>>>>>> 132390b... New function - min
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,14 @@
 #include <vector>
-<<<<<<< HEAD
-<<<<<<< HEAD
 #include <iostream>
-#include "functions.hpp"
-=======
-=======
-#include <iostream>
->>>>>>> 132390b... New function - min
 #include "../inc/functions.hpp"    // TODO: add proper compilation flag and change to #include "functions.hpp"
->>>>>>> a6c01ef... Better code layout
 
 int main() {
     std::vector<int> numbers = {142, 41, 42, 13, 0, -14, 5};
     print(numbers);
     sort(numbers);
     print(numbers);
-<<<<<<< HEAD
-<<<<<<< HEAD
     auto minimum = min(numbers);
     std::cout << "Minimum: " << minimum << '\n';
-    reverse(numbers);
-    print(numbers);
-=======
->>>>>>> a6c01ef... Better code layout
-=======
-    auto minimum = min(numbers);
-    std::cout << "Minimum: " << minimum << '\n';
->>>>>>> 132390b... New function - min
     
     return 0;
 }
