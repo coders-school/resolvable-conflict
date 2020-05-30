42570787b5e59314199a85eb8a11d13f8a1370fa
Tal <natalia.nowacka.it@gmail.com>
2020-05-30 19:41:28 +0200

    flag step done

--- inc/functions.hpp
+++ inc/functions.hpp
@@ -2,9 +2,6 @@
 
 void print(std::vector<int> const &);
 void sort(std::vector<int> &);
-<<<<<<< HEAD:inc/functions.hpp
 int min(std::vector<int> const &);
-=======
 void reverse(std::vector<int> &);
->>>>>>> reverse:functions.hpp
 
