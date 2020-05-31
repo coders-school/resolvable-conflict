66f2c2457dad773b0e7f655b17e05d48946975ea
Tal <natalia.nowacka.it@gmail.com>
2020-05-31 14:59:45 +0200

    reresolve files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 #include <iostream>
-#include "../inc/functions.hpp"    // TODO: add proper compilation flag and change to #include "functions.hpp"
+#include "functions.hpp"
 
 int main() {
     std::vector<int> numbers = {142, 41, 42, 13, 0, -14, 5};
@@ -9,6 +9,8 @@ int main() {
     print(numbers);
     reverse(numbers);
     print(numbers);
+    auto minimum = min(numbers);
+    std::cout << "Minimum: " << minimum << '\n';
     
     return 0;
 }
