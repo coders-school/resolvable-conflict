98f92136accf1ed38170cd74e086d3d2f205ca4e
MechaniQ1 <koondzik333@gmail.com>
2020-06-05 13:59:16 +0200

    Flags added

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 
-#include "../inc/functions.hpp"  // TODO: add proper compilation flag and change to #include "functions.hpp"
+#include "functions.hpp"  // TODO: add proper compilation flag and change to #include "functions.hpp"
 
 int main() {
     std::vector<int> numbers = {142, 41, 42, 13, 0, -14, 5};
