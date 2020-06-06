9db93c6bc17f52096eb73e07da670d2d04b4d23e
kali-03 <mkaliszczuk03@wp.pl>
2020-06-06 14:54:07 +0200

    Improve merge branches master and reverse

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main() {
     print(numbers);
     auto minimum = min(numbers);
     std::cout << "Minimum: " << minimum << '\n';
-    reverse(numbers);
-    print(numbers);
+    
+   
     return 0;
 }
