78df1061166e9558c221cbcae3f58589187d75f2
kari003 <kgebski68@gmail.com>
2020-06-07 17:35:32 +0000

    I added removed function min

--- src/functions.cpp
+++ src/functions.cpp
@@ -12,9 +12,14 @@ void print(std::vector<int> const & numbers) {
 void sort(std::vector<int> & unsorted) {
     std::sort(begin(unsorted), end(unsorted));
 }
-   
-void reverse(std::vector<int> & data) {
-    std::reverse(begin(data), end(data));
-}
-   
 
+int min(std::vector<int> const & values) {
+   int minimum = std::numeric_limits<int>::max();
+   for (const auto & value : values) {
+       if (value < minimum) {
+           minimum = value;
+       }
+   }
+
+return minimum;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <vector>
 #include <iostream>
-#include "functions.hpp"    // TODO: add proper compilation flag and change to #include "functions.hpp"
+#include "functions.hpp"  
 
 int main() {
     std::vector<int> numbers = {142, 41, 42, 13, 0, -14, 5};
     print(numbers);
     sort(numbers);
     print(numbers);
-    reverse(numbers);
-    print(numbers);
+    auto minimum = min(numbers);
+    std::cout << "Minimum: " << minimum << '\n';
+    
     
     return 0;
 }
